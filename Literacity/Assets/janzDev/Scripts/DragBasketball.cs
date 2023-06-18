c5fee1d2a58621caf127ffeefbc8856e209780dd
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-06-18 16:52:21 -0400

    Added drag functionality

--- Literacity/Assets/janzDev/Scripts/DragBasketball.cs
+++ Literacity/Assets/janzDev/Scripts/DragBasketball.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class DragBasketball : MonoBehaviour
+public class DragBasketball : MonoBehaviour /*, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerDownHandler*/
 {
     [Header("Variables")]
     public bool isPressed = false;
-    [SerializeField] LayerMask layer;
+    [SerializeField] LayerMask startLayer;
+    [SerializeField] LayerMask endLayer;
     Ray ray;
     RaycastHit hit;
+    public Vector3 startDragPoint = Vector3.zero;
+    public Vector3 endDragPoint = Vector3.zero;
+    public float minimumDragDistance = 5f;
 
     [Header("References")]
     public BasketballLauncher launcher;
@@ -20,14 +25,55 @@ public class DragBasketball : MonoBehaviour
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);            //converts mouse position to screenspace
 
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity, layer))            //checks for a raycast hit
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, startLayer))            //checks for a raycast hit
             {
-
-                launcher.Launch();
-                TargetCheck.CheckTarget();
+                startDragPoint = hit.point;
 
                 Debug.Log(hit.collider.name);
             }
         }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Mouse Up");
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);            //converts mouse position to screenspace
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, endLayer))            //checks for a raycast hit
+            {
+                endDragPoint = hit.point;
+
+                Debug.Log(hit.collider.name);
+
+                float difference = endDragPoint.y - startDragPoint.y;
+                difference = Mathf.Abs(difference); 
+                Debug.Log(difference); 
+
+                if(difference >= minimumDragDistance)
+                {
+                    launcher.Launch();
+                    TargetCheck.CheckTarget();
+                }
+            }
+        }
     }
+
+    // public void OnPointerDown(PointerEventData eventData)
+    // {
+    //     Debug.Log("Pointer Down");
+    // }
+
+    // public void OnDrag(PointerEventData eventData)
+    // {
+    //     Debug.Log("Dragging");
+    // }
+
+    // public void OnBeginDrag(PointerEventData eventData)
+    // {
+    //     Debug.Log("Begin Drag");
+    // }
+
+    // public void OnEndDrag(PointerEventData eventData)
+    // {
+    //     Debug.Log("End Drag");
+    // }
 }
