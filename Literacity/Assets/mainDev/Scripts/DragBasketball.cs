11a7a111a715da67723fe781e84a398e4a703def
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-06-26 12:06:47 -0400

    Added Drag fill bar for UI

--- Literacity/Assets/mainDev/Scripts/DragBasketball.cs
+++ Literacity/Assets/mainDev/Scripts/DragBasketball.cs
@@ -12,17 +12,26 @@ public class DragBasketball : MonoBehaviour
     [SerializeField] LayerMask endLayer;
     Ray ray;
     RaycastHit hit;
+
+    [Header("Darg Variables")]
     public Vector3 startDragPoint = Vector3.zero;
     public Vector3 endDragPoint = Vector3.zero;
     public float minimumDragDistance = 5f;
+
+    public float dragValue = 0f;
 
     [Header("References")]
     public BasketballLauncher launcher;
     public TargetCheck targetChecker;
 
+    [Header("UI References")]
+    public Image dragMeter;
+
     void Start()
     {
         targetChecker = FindObjectOfType<TargetCheck>();
+
+        dragMeter.fillAmount = 0f;
     }
     
     void Update()
@@ -37,25 +46,32 @@ public class DragBasketball : MonoBehaviour
             }
         }
 
-        if(Input.GetMouseButtonUp(0))
+        if(Input.GetMouseButton(0))
         {
-            Debug.Log("Mouse Up");
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);            //converts mouse position to screenspace
 
             if(Physics.Raycast(ray, out hit, Mathf.Infinity, endLayer))            //checks for a raycast hit
             {
                 endDragPoint = hit.point;
 
-                Debug.Log(hit.collider.name);
+                dragValue = endDragPoint.y - startDragPoint.y;
 
-                float difference = endDragPoint.y - startDragPoint.y;
-                Debug.Log(difference); 
+                float normalDragValue = dragValue/minimumDragDistance;
 
-                if(difference <= minimumDragDistance)
-                {
-                    launcher.Launch();
-                    targetChecker.StartCoroutine(targetChecker.CheckTarget());
-                }
+                dragMeter.fillAmount = normalDragValue;
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Mouse Up");
+
+            if(dragValue <= minimumDragDistance)
+            {
+                launcher.Launch();
+                targetChecker.StartCoroutine(targetChecker.CheckTarget());
+
+                dragMeter.fillAmount = 0f;
             }
         }
     }
