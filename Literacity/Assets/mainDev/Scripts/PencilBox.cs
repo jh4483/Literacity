347ab332feb7ae9a53ea30d55d5bed92d0521620
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-07-17 01:41:30 -0400

    Finished Animation for box Open and Close

--- Literacity/Assets/mainDev/Scripts/PencilBox.cs
+++ Literacity/Assets/mainDev/Scripts/PencilBox.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class PencilBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector2 closedPos;
+    [SerializeField] private Vector2 openPos;
+    [SerializeField] private float lidSpeed = 1f;
+    [SerializeField] private bool isRunning = false;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool isClosed = true;
+
+    public enum PencilBoxState
     {
-        
+        Closed,
+        Open
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private PencilBoxState pencilBoxState;
+
+    private void Start() 
     {
-        
+        animator = GetComponent<Animator>();
+
+        closedPos = transform.localPosition;
+    }
+
+    public void OpenBox()
+    {
+        if(animator.GetBool("isOpen"))
+        {
+            animator.SetBool("isOpen", false);
+        }
+        else if(!animator.GetBool("isOpen"))
+        {
+            animator.SetBool("isOpen", true);
+        }
+    }
+
+
+
+
+    void LidHandler()
+    {
+        switch (pencilBoxState)
+        {
+            case PencilBoxState.Closed:
+
+                transform.localPosition = Vector2.Lerp(transform.localPosition, openPos, lidSpeed * Time.deltaTime);
+
+                if((Vector2)transform.localPosition == openPos)
+                {
+                    pencilBoxState = PencilBoxState.Open;
+                    isRunning = false;
+                }
+
+            break;
+
+            case PencilBoxState.Open:
+
+                transform.localPosition = Vector2.Lerp(transform.localPosition, closedPos, lidSpeed * Time.deltaTime);
+                
+                if((Vector2)transform.localPosition == closedPos)
+                {
+                    pencilBoxState = PencilBoxState.Closed;
+                    isRunning = false;
+                }
+
+            break;
+            
+            default:
+            break;
+        }
     }
 }
