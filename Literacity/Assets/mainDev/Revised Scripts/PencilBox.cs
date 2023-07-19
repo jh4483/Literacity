b1a14c65db4c2e6aebd393cd2a12f0ea80b28d91
Janani Harinarayanan <jh4483@tc.columbia.edu>
2023-07-19 10:54:58 -0700

    Pencil state script updated

---
+++ Literacity/Assets/mainDev/Revised Scripts/PencilBox.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PencilBox : MonoBehaviour
+{
+    // [SerializeField] private Vector2 closedPos;
+    // [SerializeField] private Vector2 openPos;
+    // [SerializeField] private float lidSpeed = 1f;
+    // [SerializeField] private bool isRunning = false;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool isClosed = true;
+
+    // public enum PencilBoxState
+    // {
+    //     Closed,
+    //     Open
+    // }
+
+    // [SerializeField] private PencilBoxState pencilBoxState;
+
+    private void Start() 
+    {
+        animator = GetComponent<Animator>();
+
+        // closedPos = transform.localPosition;
+    }
+
+    public void OpenBox()
+    {
+        if(animator.GetBool("isOpen"))
+        {
+            animator.SetBool("isOpen", false);
+        }
+        else if(!animator.GetBool("isOpen"))
+        {
+            animator.SetBool("isOpen", true);
+        }
+    }
+
+
+
+
+    // void LidHandler()
+    // {
+    //     switch (pencilBoxState)
+    //     {
+    //         case PencilBoxState.Closed:
+
+    //             transform.localPosition = Vector2.Lerp(transform.localPosition, openPos, lidSpeed * Time.deltaTime);
+
+    //             if((Vector2)transform.localPosition == openPos)
+    //             {
+    //                 pencilBoxState = PencilBoxState.Open;
+    //                 isRunning = false;
+    //             }
+
+    //         break;
+
+    //         case PencilBoxState.Open:
+
+    //             transform.localPosition = Vector2.Lerp(transform.localPosition, closedPos, lidSpeed * Time.deltaTime);
+                
+    //             if((Vector2)transform.localPosition == closedPos)
+    //             {
+    //                 pencilBoxState = PencilBoxState.Closed;
+    //                 isRunning = false;
+    //             }
+
+    //         break;
+            
+    //         default:
+    //         break;
+    //     }
+    // }
+}
