1e66fb115a5c9609cb29b5fae670c493c2a7b273
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-07-19 20:02:12 -0400

    Cleaned up PencilBox Script

--- Literacity/Assets/mainDev/Revised Scripts/PencilBox.cs
+++ Literacity/Assets/mainDev/Revised Scripts/PencilBox.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class PencilBox : MonoBehaviour
 {
-    // [SerializeField] private Vector2 closedPos;
-    // [SerializeField] private Vector2 openPos;
-    // [SerializeField] private float lidSpeed = 1f;
-    // [SerializeField] private bool isRunning = false;
-
     [SerializeField] private Animator animator;
-    [SerializeField] private bool isClosed = true;
-
-    // public enum PencilBoxState
-    // {
-    //     Closed,
-    //     Open
-    // }
-
-    // [SerializeField] private PencilBoxState pencilBoxState;
 
     private void Start() 
     {
         animator = GetComponent<Animator>();
-
-        // closedPos = transform.localPosition;
     }
 
     public void OpenBox()
@@ -38,40 +22,4 @@ public class PencilBox : MonoBehaviour
             animator.SetBool("isOpen", true);
         }
     }
-
-
-
-
-    // void LidHandler()
-    // {
-    //     switch (pencilBoxState)
-    //     {
-    //         case PencilBoxState.Closed:
-
-    //             transform.localPosition = Vector2.Lerp(transform.localPosition, openPos, lidSpeed * Time.deltaTime);
-
-    //             if((Vector2)transform.localPosition == openPos)
-    //             {
-    //                 pencilBoxState = PencilBoxState.Open;
-    //                 isRunning = false;
-    //             }
-
-    //         break;
-
-    //         case PencilBoxState.Open:
-
-    //             transform.localPosition = Vector2.Lerp(transform.localPosition, closedPos, lidSpeed * Time.deltaTime);
-                
-    //             if((Vector2)transform.localPosition == closedPos)
-    //             {
-    //                 pencilBoxState = PencilBoxState.Closed;
-    //                 isRunning = false;
-    //             }
-
-    //         break;
-            
-    //         default:
-    //         break;
-    //     }
-    // }
 }
