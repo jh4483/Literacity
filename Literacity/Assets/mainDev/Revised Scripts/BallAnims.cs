a6c8437e5423bf57d2952200cce53e6158b2904e
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-08-08 22:12:25 -0400

    Wrapped up Kaz ANims

---
+++ Literacity/Assets/mainDev/Revised Scripts/BallAnims.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnims : MonoBehaviour
+{
+    [SerializeField] GameObject ballSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShootBallL()
+    {
+        
+    }
+
+    public void ShootBallDribbleL()
+    {
+
+    }
+
+    public void ShootBallDribbleR()
+    {
+
+    }
+}
