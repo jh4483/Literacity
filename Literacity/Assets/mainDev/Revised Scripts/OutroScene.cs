6e600fb52e0738fbbb6c65810b7083278fe01eb7
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-08-10 16:51:35 -0400

    Synced outro audio

--- Literacity/Assets/mainDev/Revised Scripts/OutroScene.cs
+++ Literacity/Assets/mainDev/Revised Scripts/OutroScene.cs
@@ -18,16 +18,16 @@ public class OutroScene : MonoBehaviour
     IEnumerator LoadOutroScene()
     {
         GetComponent<AudioSource>().Play();
-        yield return new WaitForSeconds(1.2f);
+        yield return new WaitForSeconds(4.5f);
         transform.GetChild(0).gameObject.SetActive(true);   
 
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(1.0f);
         transform.GetChild(1).gameObject.SetActive(true);  
         
-        yield return new WaitForSeconds(1.4f);
+        yield return new WaitForSeconds(1.0f);
         transform.GetChild(2).gameObject.SetActive(true); 
 
-        yield return new WaitForSeconds(1.4f);
+        yield return new WaitForSeconds(1.0f);
         transform.GetChild(3).gameObject.SetActive(true);  
 
     }
