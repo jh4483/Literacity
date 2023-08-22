06a8fd374033a9266bcb3dccf4395f12a6b3997c
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-08-22 18:53:14 -0400

    Created simple rectangle collision

---
+++ Literacity/Assets/mainDev/Revised Scripts/BackboardDrag.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackboardDrag : MonoBehaviour 
+{
+    public Image backboardImage;
+    public float width = 10f;
+
+    void Update()
+    {
+        CustomCollisionCheck();
+    }
+
+    void CustomCollisionCheck()
+    {
+        // if( Input.mousePosition.x > backboardImage.rectTransform.position.x - backboardImage.rectTransform.rect.width / 2 &&
+        //     Input.mousePosition.x < backboardImage.rectTransform.position.x - backboardImage.rectTransform.rect.width / 2 + (width) ||
+        //     Input.mousePosition.x < backboardImage.rectTransform.position.x + backboardImage.rectTransform.rect.width / 2 &&
+        //     Input.mousePosition.x > backboardImage.rectTransform.position.x + backboardImage.rectTransform.rect.width / 2 - (width) &&
+        //     Input.mousePosition.y > backboardImage.rectTransform.position.y - backboardImage.rectTransform.rect.height /2 &&
+        //     Input.mousePosition.y < backboardImage.rectTransform.position.y - backboardImage.rectTransform.rect.height /2 + (width) ||
+        //     Input.mousePosition.y < backboardImage.rectTransform.position.y + backboardImage.rectTransform.rect.height /2 &&
+        //     Input.mousePosition.y > backboardImage.rectTransform.position.y + backboardImage.rectTransform.rect.height /2 - (width)
+        //     )
+        // {
+        //     Debug.Log("HIT");
+        // }
+
+        // if( Input.mousePosition.x > backboardImage.rectTransform.position.x - backboardImage.rectTransform.rect.width / 2 &&
+        //     Input.mousePosition.x < backboardImage.rectTransform.position.x - backboardImage.rectTransform.rect.width / 2 + (width)
+        //     )
+        // {
+        //     Debug.Log("Collision Left");
+        // }
+
+        // if( Input.mousePosition.x < backboardImage.rectTransform.position.x + backboardImage.rectTransform.rect.width / 2 &&
+        //     Input.mousePosition.x > backboardImage.rectTransform.position.x + backboardImage.rectTransform.rect.width / 2 - (width)
+        //     )
+        // {
+        //     Debug.Log("Collision Right");
+        // }
+
+        // if( Input.mousePosition.y > backboardImage.rectTransform.position.y - backboardImage.rectTransform.rect.height /2 &&
+        //     Input.mousePosition.y < backboardImage.rectTransform.position.y - backboardImage.rectTransform.rect.height /2 + (width)
+        //     )
+        // {
+        //     Debug.Log("Collision Bottom");            
+        // }
+        // if( Input.mousePosition.y < backboardImage.rectTransform.position.y + backboardImage.rectTransform.rect.height /2 &&
+        //     Input.mousePosition.y > backboardImage.rectTransform.position.y + backboardImage.rectTransform.rect.height /2 - (width)
+        //     )
+        // {
+        //     Debug.Log("Collision Top");
+        // }
+
+        if( Input.mousePosition.x > backboardImage.rectTransform.position.x - backboardImage.rectTransform.rect.width / 2 &&
+            Input.mousePosition.x < backboardImage.rectTransform.position.x + backboardImage.rectTransform.rect.width / 2 &&
+            Input.mousePosition.y > backboardImage.rectTransform.position.y - backboardImage.rectTransform.rect.height /2 &&
+            Input.mousePosition.y < backboardImage.rectTransform.position.y + backboardImage.rectTransform.rect.height /2)
+        {
+            Debug.Log("Collision");
+        }
+    }
+}
