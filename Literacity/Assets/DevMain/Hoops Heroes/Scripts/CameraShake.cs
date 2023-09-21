98895388fa19a7d5f5257991268dbff118124c6c
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2023-09-21 01:41:51 -0400

    Finished Camera Shake

--- Literacity/Assets/DevMain/Hoops Heroes/Scripts/CameraShake.cs
+++ Literacity/Assets/DevMain/Hoops Heroes/Scripts/CameraShake.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraShake : MonoBehaviour
 {
     public AnimationCurve curve;
+    //public Canvas canvas;
+    //public Image background;
     public float duration = 1f;
     public Vector3 startPos;
     public float time = 0f;
+    public float strengthMultiplier = 1f;
     public bool isShaking = false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             if(isShaking)
             {
@@ -27,16 +31,18 @@ public class CameraShake : MonoBehaviour
         isShaking = true;
         startPos = transform.localPosition;
         time = 0f;
+        //canvas.renderMode = RenderMode.WorldSpace;
 
         while(time <= duration)
         {
             time += Time.deltaTime;
-            float strength = curve.Evaluate(time / duration);
+            float strength = curve.Evaluate(time / duration) * strengthMultiplier;
             transform.localPosition = startPos + Random.insideUnitSphere * strength;
             yield return null;
         }
 
         transform.localPosition = startPos;
+        //canvas.renderMode = RenderMode.ScreenSpaceCamera;
         isShaking = false;
     }
 }
