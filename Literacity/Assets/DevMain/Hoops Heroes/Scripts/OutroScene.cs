856cebcde2409621916a3065e018a62a26256114
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2023-09-22 18:14:58 -0400

    Adding Scriptable Objects for CameraShake

--- Literacity/Assets/DevMain/Hoops Heroes/Scripts/OutroScene.cs
+++ Literacity/Assets/DevMain/Hoops Heroes/Scripts/OutroScene.cs
@@ -6,14 +6,20 @@ public class OutroScene : MonoBehaviour
 {
     public GameObject outroScene;
     public GameObject outroAudio;
+
+    public ParticleSystem[] particles;
+    public float time = 0.1f;
+    public float repeatRate = 0.3f;
     void Start()
     {
         StartCoroutine(LoadOutroScene());
+        InvokeRepeating("LoopParticles", time, repeatRate);
     }
 
     IEnumerator LoadOutroScene()
     {
         GetComponent<AudioSource>().Play();
+        LoopParticles();
         yield return new WaitForSeconds(4.5f);
         transform.GetChild(0).gameObject.SetActive(true);   
 
@@ -26,5 +32,11 @@ public class OutroScene : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
         transform.GetChild(3).gameObject.SetActive(true);  
 
+    }
+
+    void LoopParticles()
+    {
+        int count = Random.Range(0, particles.Length);
+        particles[count].Play();
     }
 }
