6724aac416612b27e49e8e3e32f56b8ae76b4017
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2023-11-15 14:21:10 -0500

    Debugged Drag and Drop

--- Literacity/Assets/Dev/Akshay/AB_HH_TechTest/HH_Scripts/HH_DragNDrop.cs
+++ Literacity/Assets/Dev/Akshay/AB_HH_TechTest/HH_Scripts/HH_DragNDrop.cs
@@ -7,33 +7,38 @@ public class HH_DragNDrop : MonoBehaviour
     public Camera targetCamera;
 
     [Header("Mouse Stuff")]
-    public Vector3 w2sPoint;        // World to Screen Point
+    public float zySense;
+    public float heightMultiplier;
     public Vector3 mPosInWorld;
     public Vector3 mDragPos;
-    public Vector3 inMPos;
 
-    Vector3 GetMousePos()
+    Vector3 GetObjectPos()
     {
-        w2sPoint = targetCamera.WorldToScreenPoint(transform.position);
-
-        return w2sPoint;
+        // Converts the World Position of the Object to Screen Position
+        return targetCamera.WorldToScreenPoint(transform.position);
     }
 
     void OnMouseDown()
     {
-        inMPos = Input.mousePosition;
-
-        mPosInWorld = Input.mousePosition - GetMousePos();
+        mPosInWorld = Input.mousePosition - GetObjectPos();
+        //mPosInWorld.x = mPosInWorld.x / Screen.width;
+        //mPosInWorld.y = mPosInWorld.y / Screen.height;
     }
 
     void OnMouseDrag()
     {
-        mDragPos = targetCamera.ScreenToWorldPoint(Input.mousePosition - mPosInWorld);
+
+        Vector3 flyingVec = Input.mousePosition - mPosInWorld;
+
+        //Backbox math which sets the starting position for the object and adds as the mouse is moved up to sync Z and Y
+        zySense = Input.mousePosition.y / Screen.height;
+        float newZPos = flyingVec.y * zySense/heightMultiplier;
+
+        //float newZPos = flyingVec.z + flyingVec.y - (flyingVec.y * zySense);
+        //float newZPos = flyingVec.z + flyingVec.y - zySense;
+
+        mDragPos = targetCamera.ScreenToWorldPoint(new Vector3(flyingVec.x, flyingVec.y, newZPos));
         
         transform.position = mDragPos;
     }
-
-
-
-
 }
