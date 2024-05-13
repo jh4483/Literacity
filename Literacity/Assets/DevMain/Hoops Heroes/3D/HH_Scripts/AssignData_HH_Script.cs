a72b666954eeb823af61ebd12751d34ed41ac11c
Janani Harinarayanan <jh4483@tc.columbia.edu>
2024-05-13 16:42:31 -0700

    new dictionary updated

--- Literacity/Assets/DevMain/Hoops Heroes/3D/HH_Scripts/AssignData_HH_Script.cs
+++ Literacity/Assets/DevMain/Hoops Heroes/3D/HH_Scripts/AssignData_HH_Script.cs
@@ -11,10 +11,15 @@ public class AssignData_HH_Script : MonoBehaviour
     [SerializeField]
     public GameObject[] buttonArray;
     public Dictionary<GameObject, string> buttonCollection = new Dictionary<GameObject, string>();
-    ObtainData_HH_Script obtainData;
+    [SerializeField]
+    private GameObject[] hotspotButtons;
+    private Dictionary<GameObject, GameObject> hotspotLink = new Dictionary<GameObject, GameObject>();
+    private ObtainData_HH_Script obtainData;
+
     void Start()
     {
         obtainData = FindObjectOfType<ObtainData_HH_Script>();
+        InitializeHotspotLinks();
     }
 
     void Update()
@@ -22,9 +27,19 @@ public class AssignData_HH_Script : MonoBehaviour
         
     }
 
+    private void InitializeHotspotLinks()
+    {
+        for (int i = 0; i < buttonArray.Length; i++)
+        {
+            hotspotLink.Add(buttonArray[i], hotspotButtons[i]);
+            Debug.Log(hotspotLink[buttonArray[i]]);
+        }
+   
+    }
+
     public void AddBallData()
     {
-        for(int i = 0; i < obtainData.ballValues.Count; i++)
+        for (int i = 0; i < obtainData.ballValues.Count; i++)
         {
             ballCollection[i].GetComponent<TextMeshProUGUI>().text = obtainData.ballValues[i];
         }
@@ -32,10 +47,9 @@ public class AssignData_HH_Script : MonoBehaviour
 
     public void AddButtonData()
     {
-        for(int i = 0; i < obtainData.buttonValues.Count; i++)
+        for (int i = 0; i < obtainData.buttonValues.Count; i++)
         {
             buttonCollection.Add(buttonArray[i], obtainData.buttonValues[i]);
         }
     }
-
 }
